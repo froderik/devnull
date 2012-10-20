dffcccbe14411166613fb70a59d01423bd05ce36
Andreas Svanberg <ante.svanberg@gmail.com>
2012-10-20 15:29:43 +0200

    intro maphandler

--- packthemap/script.js
+++ packthemap/script.js
@@ -1,29 +1,79 @@
-$(document).ready(function(){
+var maphandler = function(){
+
+	var matrixsize = 50;
+
+	var gamemap = [50][50];
+
 
 	function init_map() {
 		console.log("init map");
 		
 		$('mapDiv').html('');
 
-		for (y = 0; y<50;y++) {
+		for (y = 0; y<matrixsize;y++) {
 			var maprow = "";
 			maprow = '<div class="row-separator">';
 
 			
-			for (x = 0; x<50;x++) {
+			for (x = 0; x<matrixsize;x++) {
 				var cellid = "" + x + "-" +  y;
-				maprow += '<span class="map-cell" id="' + cellid + '"/>';	
+				maprow += '<span class="map-cell free-cell" id="' + cellid + '"/>';	
 			}
 
 			maprow += '</div>';
 			$('#mapDiv').append(maprow);
 		}
 		
+		create_walls(3);
+	}
+
+	function create_walls(numberofvals) {
+		
+		for (i=0;i<numberofvals;i++) {
+			create_wall();
+		}
+
+		
 
 	}
 
 
-	init_map();
+	function create_wall() {
+		// 0 is horizontal, 1 i vertical
+		var cellid;
+		var direction = Math.floor(Math.random());
+		var wall_lengt = Math.floor(Math.random()* (0.3 * matrixsize));
+		var wall_start = Math.floor(Math.random()*33) + 1
+		var wall_other = Math.floor(Math.random()*48) + 1
+		
+		
+
+		for (i=wall_start;i<wall_lengt;i++) {
+			
+			if (direction == 0) {
+				cellid = "#" + i + "-" + wall_other;
+			} else {
+				cellid = "#" + wall_other + "-" + i;
+			}
+
+			
+			$(cellid).removeClass('free-cell');
+			$(cellid).addClass('wall-cell');
+		}
+
+	}
+
+	return {
+		init_map:init_map
+	};
+
+}();
+
+
+
+$(document).ready(function(){
+
+	maphandler.init_map();
 });
 
 
