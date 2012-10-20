69a7bc48744df957f0da4e003fab8bc4160094d6
Andreas Svanberg <ante.svanberg@gmail.com>
2012-10-20 16:12:37 +0200

    fix gamemap

--- packthemap/script.js
+++ packthemap/script.js
@@ -2,13 +2,27 @@ var maphandler = function(){
 
 	var matrixsize = 50;
 
-	var gamemap = [50][50];
+	var gamemap = [];
+
+	function init_game_map() {
+
+		for (var k = 0; k < matrixsize; k++) {
+			gamemap[k] = [];
+			for (var l = 0 ; l < matrixsize  ; l++) {
+				gamemap[k][l] = 'f';
+			};
+		};
+	}
 
 
 	function init_map() {
+
+		var x,y;
 		console.log("init map");
 		
 		$('mapDiv').html('');
+
+		init_game_map();	
 
 		for (y = 0; y<matrixsize;y++) {
 			var maprow = "";
@@ -24,42 +38,56 @@ var maphandler = function(){
 			$('#mapDiv').append(maprow);
 		}
 		
-		create_walls(3);
+		create_walls(10);
+
+		console.log(gamemap);
 	}
 
-	function create_walls(numberofvals) {
-		
-		for (i=0;i<numberofvals;i++) {
+	function create_walls(numberofwalls) {
+		var i = 0;
+		for(i=0;i<=numberofwalls;i++) {
 			create_wall();
 		}
-
-		
-
 	}
-
 
 	function create_wall() {
 		// 0 is horizontal, 1 i vertical
-		var cellid;
-		var direction = Math.floor(Math.random());
-		var wall_lengt = Math.floor(Math.random()* (0.3 * matrixsize));
+		var x,y;
+		var direction = Math.floor(Math.random()*10);
+		var wall_length = Math.floor(Math.random()* (0.3 * matrixsize)) + 5 ;
 		var wall_start = Math.floor(Math.random()*33) + 1
 		var wall_other = Math.floor(Math.random()*48) + 1
-		
-		
-
-		for (i=wall_start;i<wall_lengt;i++) {
-			
-			if (direction == 0) {
-				cellid = "#" + i + "-" + wall_other;
-			} else {
-				cellid = "#" + wall_other + "-" + i;
-			}
-
-			
-			$(cellid).removeClass('free-cell');
-			$(cellid).addClass('wall-cell');
+		var wall_end = wall_start + wall_length;
+		if (wall_end > 49) {
+			wall_end = 48;
 		}
+
+		console.log("direction: " + direction);
+		console.log("wall_length:" + wall_length);
+		console.log("wall_start:" + wall_start);
+		console.log("wall_other: " + wall_other);
+
+		for (i=wall_start;i<wall_end;i++) {
+			
+			if ((direction % 2) == 0) {
+				x=i;
+				y=wall_other;
+			} else {
+				x=wall_other;
+				y=i;
+			}
+			console.log("marking cell: (x/y)" + x + ":" + y);
+			mark_cell_wall(x,y);
+		}
+
+	}
+
+	function mark_cell_wall(x,y) {
+
+		var cellid = "#" + x + "-" + y;
+		$(cellid).removeClass('free-cell');
+		$(cellid).addClass('wall-cell');
+		gamemap[x][y]= 'w';
 
 	}
 
@@ -69,10 +97,7 @@ var maphandler = function(){
 
 }();
 
-
-
 $(document).ready(function(){
-
 	maphandler.init_map();
 });
 
