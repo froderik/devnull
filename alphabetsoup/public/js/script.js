bc7f566e57563766a5b42e514b197005aa859975
Andreas Svanberg <ante.svanberg@gmail.com>
2012-10-20 10:17:21 +0200

    basic inputs and buttons

--- alphabetsoup/public/js/script.js
+++ alphabetsoup/public/js/script.js
@@ -1,5 +1,25 @@
+function placeWords(words) {
+	$('#wordsBox').val(words);
+}
+
+function sendGuess() {
+	var input = $('#guessBox').val();
+	console.log("sending: " + input);
+
+}
+
 $(document).ready(function(){
-	alert('apa');
+	console.log("starting alphabetsoup client");
+
+
+
+	placeWords("apa apa apa");
+
+	$('#guessButton').click(function(){
+		console.log('guessing....');
+		sendGuess();
+	});
+
 });
 
 
