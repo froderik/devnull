e739191a5609a1497fd7820a2bd285f877c424ae
Andreas Svanberg <ante.svanberg@gmail.com>
2012-10-20 12:28:10 +0200

    post files from file selector

--- loadtheloop/public/js/script.js
+++ loadtheloop/public/js/script.js
@@ -1,5 +1,40 @@
 $(document).ready(function(){
-	alert('apa');
+
+	//var filesInput = $('#files-upload');
+
+	
+	$('#uploadButton').click(function(){
+
+
+		var fileList = document.getElementById('files-upload').files;
+
+		console.log("Getting files: " + fileList);
+
+
+		if (typeof fileList !== "undefined") {
+			for (var i=0, l=fileList.length; i<l; i++) {
+
+				console.log(fileList[i].name);
+
+				
+				$.ajax({
+      					type: "POST",
+      					url: 'http://localhost:4567/upload', 
+						enctype: 'multipart/form-data',
+      					data: {file: fileList[i].name},
+      					success: function () {
+        					console.log("Data Uploaded: ");
+      					}
+      				});
+				
+			}
+
+		}
+
+		
+	});
+
+
 });
 
 
--- loadtheloop/server.rb
+++ loadtheloop/server.rb
@@ -7,14 +7,15 @@ get '/' do
     html << "<li><a href=\"filearea/#{file}\">#{file}</a></li>" unless file == '.' or file == '..'
   }
   html << '</ul>'
-  html << '<form action="upload" method="post">'
-  html << '<input id="files-upload" type="file" multiple>'
-  html << '<input type="submit">'
-  html << '</form>'
+  html << '<input id="files-upload" type="file" multiple />'
+  html << '<button id="uploadButton">Upload files</button>'
+  html << ' <script src="jquery/jquery-1.8.2.js"></script>'
+  html << '<script src="js/script.js"></script>'
   html << '</body></html>'
   html
 end
 
 post '/upload' do
-  puts "========= #{params.inspect}"
+  puts "=== Got file: #{params[:file]}"
+  puts request.body.read
 end
