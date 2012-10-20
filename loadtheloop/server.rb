64534643c43175a909af8a3f863fd86a73100578
froderik <fredrik@eldfluga.se>
2012-10-20 11:39:40 +0200

    form

--- loadtheloop/server.rb
+++ loadtheloop/server.rb
@@ -7,7 +7,14 @@ get '/' do
     html << "<li><a href=\"filearea/#{file}\">#{file}</a></li>" unless file == '.' or file == '..'
   }
   html << '</ul>'
-
+  html << '<form action="upload" method="post">'
+  html << '<input id="files-upload" type="file" multiple>'
+  html << '<input type="submit">'
+  html << '</form>'
   html << '</body></html>'
   html
+end
+
+post '/upload' do
+  puts "========= #{params.inspect}"
 end
