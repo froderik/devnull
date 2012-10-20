0fcfd0a81e9272570763b6420f5321ed2f609e43
froderik <fredrik@eldfluga.se>
2012-10-20 10:18:20 +0200

    guessing

--- alphabetsoup/server.rb
+++ alphabetsoup/server.rb
@@ -3,15 +3,16 @@ require 'mechanize'
 require 'json'
 
 get '/10words' do
-    agent = Mechanize.new
-    page = agent.get 'http://www.dn.se'
-    paragraph_nodes = page.search('p')
-    texts = paragraph_nodes.map{ |node| node.text.split ' ' }.flatten
-    texts.sort! { |x,y| y.length <=> x.length }
-    @@ten_words = texts[0..9]
-    @@ten_words.to_json
+  agent = Mechanize.new
+  page = agent.get 'http://www.dn.se'
+  paragraph_nodes = page.search('p')
+  texts = paragraph_nodes.map{ |node| node.text.split ' ' }.flatten
+  texts.sort! { |x,y| y.length <=> x.length }
+  @@ten_words = texts[0..9]
+  @@ten_words.to_json
 end
 
-post '/guess' do
-
+get '/guess' do
+  correct = @@ten_words.detect(params[:q])
+  status 404 unless correct
 end
