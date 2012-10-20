bbbf042bf9e72df77b525b9866aeba839a48a3dc
Andreas Svanberg <ante.svanberg@gmail.com>
2012-10-20 10:45:08 +0200

    fetch words

--- alphabetsoup/public/js/script.js
+++ alphabetsoup/public/js/script.js
@@ -1,10 +1,23 @@
 function placeWords(words) {
-	$('#wordsBox').val(words);
+
+	$.getJSON('http://localhost:4567/10words', function(data) {
+	  
+		console.log("Got data:" + data);
+		$('#wordsBox').val(data.join(" "));
+	
+	});
+
 }
 
 function sendGuess() {
 	var input = $('#guessBox').val();
 	console.log("sending: " + input);
+	$.getJSON('http://localhost:4567/guess', input, function(data) {
+	  
+		console.log("Got data:" + data);
+		
+	
+	});
 
 }
 
